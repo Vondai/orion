1c33ae0bae4d44624ae1f721ead1bf9ab37efaa2
Vondai <62604050+Vondai@users.noreply.github.com>
2021-12-12 13:46:25 +0200

    Initial api create.

---
+++ api/OrionApi/OrionApi/Data/Models/Community.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrionApi.Data.Models
+{
+    public class Community
+    {
+        public Community()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Members = new HashSet<User>();
+            this.Posts = new HashSet<Post>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public ICollection<User> Members { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ api/OrionApi/OrionApi/Data/OrionDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OrionApi.Data.Models;
+
+namespace OrionApi.Data
+{
+    public class OrionDbContext : IdentityDbContext<User>
+    {
+        public OrionDbContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Community> Communities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
