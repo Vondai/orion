9a9917685c95a0c72ade24df7c7dc3eb09c59196
Vondai <62604050+Vondai@users.noreply.github.com>
2021-12-25 08:19:29 +0200

    toggle comments sorting menu

--- client/src/components/Posts/Post.js
+++ client/src/components/Posts/Post.js
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import * as postService from "../../services/postService";
 import Footer from "../Footer/Footer";
@@ -10,6 +10,7 @@ import "./Post.css";
 function Post() {
   const { isAuthenticated } = useAuth();
 
+  const sortingPicker = useRef();
   const { communityName, postId } = useParams();
   const [post, setPost] = useState({});
   useEffect(() => {
@@ -35,18 +36,25 @@ function Post() {
         Log in or sign up to leave a comment
       </span>
       <div className="post-comment-cta-wrapper">
-        <button className="post-comment-login"><Link to="/login">Log in</Link></button>
-        <button className="post-comment-signup"><Link to="/signup">Sign up</Link></button>
+        <button className="post-comment-login">
+          <Link to="/login">Log in</Link>
+        </button>
+        <button className="post-comment-signup">
+          <Link to="/signup">Sign up</Link>
+        </button>
       </div>
     </section>
   );
   const noCommentsSection = (
-    <div className='no-comments-wrapper'>
+    <div className="no-comments-wrapper">
       <i className="fas fa-comments"></i>
-      <p className='no-comments-text'>No Comments Yet</p>
-      <p className='no-comments-info'>Be the first to share what you think!</p>
+      <p className="no-comments-text">No Comments Yet</p>
+      <p className="no-comments-info">Be the first to share what you think!</p>
     </div>
   );
+  function sortingClickBtnHandler() {
+    sortingPicker.current.classList.toggle('active');
+  }
   return (
     <main className="post">
       <section className="post-content">
@@ -77,27 +85,37 @@ function Post() {
           communityName={communityName}
         />
         {isAuthenticated ? leaveCommentArea : guestNavigation}
-        <section className='post-comments-sorting-wrapper'>
-          <div className='comments-sorting-picker'>
-            <button className='comments-sorting-btn' type='button'>
-              <span className='comments-sorting-text'>Sort by: Best</span>
+        <section className="post-comments-sorting-wrapper">
+          <div className="comments-sorting-picker">
+            <button
+              className="comments-sorting-btn"
+              type="button"
+              onClick={sortingClickBtnHandler}
+            >
+              <span className="comments-sorting-text">Sort by: Best</span>
             </button>
             <i className="fas fa-sort-down"></i>
           </div>
+          <div className="sorting-picker" ref={sortingPicker}>
+            <Link to="?sort=best" className="sorting-item">
+              <button className='sorting-item-btn'>Best</button>
+            </Link>
+            <Link to="?sort=top" className="sorting-item">
+              <button className='sorting-item-btn'>Top</button>
+            </Link>
+            <Link to="?sort=new" className="sorting-item">
+              <button className='sorting-item-btn'>New</button>
+            </Link>
+            <Link to="?sort=old" className="sorting-item">
+              <button className='sorting-item-btn'>Old</button>
+            </Link>
+          </div>
         </section>
-        <div className='sorting-picker'>
-          <button className='sorting-item'><Link to='?sort=Best'>Best</Link></button>
-          <button className='sorting-item'><Link to='?sort=Best'>Top</Link></button>
-          <button className='sorting-item'><Link to='?sort=new'>New</Link></button>
-          <button className='sorting-item'><Link to='?sort=old'>Old</Link></button>
-        </div>
         <section className="post-comments-wrapper">
-          {post.commentsCount === 0 ? 
-          noCommentsSection :
-          < Comment />}
+          {post.commentsCount === 0 ? noCommentsSection : <Comment />}
         </section>
       </section>
-      <div className='side-wrapper'>
+      <div className="side-wrapper">
         <Footer />
       </div>
     </main>
